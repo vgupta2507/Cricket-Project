a5145fc651dadc3cf08389ce166e793b104ee9bc
Vaibhav Gupta <vgupta2507@gmail.com>
2016-01-18 15:30:16 +0530

    First useful example added- Vaibhav

--- TESTS/Getting A Player's Recent Record/test.py
+++ TESTS/Getting A Player's Recent Record/test.py
@@ -6,9 +6,26 @@ dhawan = urllib.request.urlopen("http://www.espncricinfo.com/india/content/playe
                                  # 'dhawan' holds the source code of webpage now, but as a request page
 dhawan_html = str(dhawan.read())                                                     
                                  # dhawan_html => The source code of Dhawan page, as a str object
-in_file = open('dhawan_page.txt', 'r')                                               
-in_file.write(dhawan_html)       # Writing that dtaa to a file in case we require it later
+in_file = open('dhawan_page.txt', 'w')                                               
+in_file.write(dhawan_html)       # Writing that data to a file in case we require it later
 
-small_segment = re.findall('<table/sclass="engineTable">.*?</table>', dhawan_html) 
+dhawan_table = re.findall('<table class="engineTable">.*?</table>', dhawan_html) 
                                  # Extracts the table out of the html doc
-pprint(small_segment)            # Hopefully cleanly prints to our screen
+print(len(dhawan_table))            # Hopefully cleanly prints to our screen, and it does
+out_file = open('dhawan_recent.txt', 'w')
+                                 # To store the table, actually not needed, but still if need arises
+out_file.write(dhawan_table[3])
+                                 # The webpage has 4 such tables, but our interest lies in only the 4th one
+runs_list_tags = re.findall('<td class="left" nowrap="nowrap">[0-9]+</td>', dhawan_table[3])
+                                 # We store all the tags that contain runs
+pprint(runs_list_tags)           # Just for checking
+
+runs = []
+for tag in runs_list_tags:
+   runs.append( int( re.findall('[0-9]+', tag)[0] ) )
+   
+pprint(runs)
+
+
+
+# So, if given a player's html page we should be able to return a list containing his recent runs, and maybe also the bowling figures
